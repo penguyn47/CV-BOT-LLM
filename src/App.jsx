f086e75e5fd18e84af26aa388ea7496f03f0301a
MrPen <lan474444@gmail.com>
2025-06-06 16:19:30 +0700

    Modified Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { Navbar } from './components'
+import { CVTemplate, Home } from './pages'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import CVPreview from './pages/CVPreview'
+
 function App() {
-  return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+	return (
+		<>
+			<Router>
+				{/* HEADER - SECTION */}
+				<Navbar />
+
+				{/* BODY - SECTION */}
+				<Routes>
+					<Route path="/" element={<Home></Home>}></Route>
+					<Route path="/cvtemplate" element={<CVTemplate></CVTemplate>}></Route>
+					<Route path="/cvpreview" element={<CVPreview></CVPreview>}></Route>
+				</Routes>
+			</Router>
+		</>
+	)
 }
 
-export default App;
+export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,44 @@
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
-return <>
-    <div className="flex justify-between py-4 px-8 border-b-1">
-        {/* PAGE ICON SECTION */}
-        <div className="flex justify-between gap-2 items-center">
-            <img src="/icon.png" className="h-[30px]" />
-            <div className="font-bold text-xl">CV Pro</div>
-        </div>
+	return (
+		<>
+			<div className="flex justify-between py-4 px-8 border-b-3 border-gray-300">
+				{/* PAGE ICON SECTION */}
+				<div className="flex justify-between gap-2 items-center">
+					<img src="/icon.png" className="h-[30px]" />
+					<div className="font-bold text-xl">CV Pro</div>
+				</div>
 
-        {/* NAV ITEMS SECTION */}
-        <div>
-            <ul className="flex justify-between gap-6 text-xl">
-                <li>
-                    <div>
-                        Trang chủ
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        Mẫu CV
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        Hồ sơ của tôi
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        Hướng dẫn
-                    </div>
-                </li>
-            </ul>
-        </div>
+				{/* NAV ITEMS SECTION */}
+				<div>
+					<ul className="flex justify-between gap-6 text-xl">
+						<li>
+							<NavLink className="hover:text-gray-400" to="/" end>
+								Trang chủ
+							</NavLink>
+						</li>
+						<li>
+							<NavLink className="hover:text-gray-400" to="/cvtemplate" end>
+								Mẫu CV
+							</NavLink>
+						</li>
+						<li>
+							<div>Hồ sơ của tôi</div>
+						</li>
+						<li>
+							<div>Hướng dẫn</div>
+						</li>
+					</ul>
+				</div>
 
-        {/* USER SECTION */}
-        <div>
-            <img src="/icon.png" className="h-[30px]"/>
-        </div>
-    </div>
-  </>
+				{/* USER SECTION */}
+				<div>
+					<img src="/icon.png" className="h-[30px]" />
+				</div>
+			</div>
+		</>
+	)
 }
 
-export default Navbar;
+export default Navbar
