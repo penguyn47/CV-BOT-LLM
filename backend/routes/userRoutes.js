652a6253c5d6a01eb08b10ede8cdf19220236ad0
MrPen <lan474444@gmail.com>
2025-07-05 16:58:31 +0700

    clear

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-router.get('/users', userController.getUser);
-router.post('/users', userController.createUser);
-router.get('/layout', userController.getLayout); // Thêm route mới
-
-module.exports = router;
